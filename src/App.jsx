b5886360d3f8abfc392c568e413121fdb4391d0c
dragon-slayer99 <guudurumahesh@gmail.com>
2025-10-05 23:56:07 +0530

    Update index.html, package.json, and package-lock.json for deployment setup
    - Commented out the React Scan script in index.html.
    - Updated favicon path in index.html.
    - Added homepage URL to package.json.
    - Included gh-pages dependency in package.json and package-lock.json.
    - Configured predeploy and deploy scripts in package.json for GitHub Pages deployment.

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,10 @@ import AUDIO from '/Audios/wishesAudio.mp3'
 const HeroSection = lazy(() => import('./Components/HeroSection/HeroSection'))
 const TextCursor = lazy(() => import('./Components/Cursor/TextCursor'))
 const DomeGallery = lazy(() => import('./Components/DomeGallery/DomeGallery'))
-// const WordsFromHeart = lazy(() => import('./Components/WordsFromHeart/Accordian/WordsFromHeart'))
 const FinalWords = lazy(() => import('./Components/FinalWords/FinalWords'))
 const LoveQuotes = lazy(() => import('./Components/WordsFromHeart/MyLoveForYou/LoveQuotes'))
 const Demo = lazy(() => import('./Components/WishingAudio/AudioPlayer'))
 const Accordian = lazy(() => import('./Components/WordsFromHeart/Accordian/Accordian'))
-const EmojiTrailCursor = lazy(() => import('./Components/ExpCursor/EmojiTrailCursor'))
 const Letter = lazy(() => import('./Components/WordsFromHeart/Letter/Letter'))
 const Footer = lazy(() => import('./Components/Footer/Footer'))
 
@@ -19,12 +17,12 @@ function App() {
   return (
     <>
       <Suspense fallback={null}>
-        {/* <TextCursor
+        <TextCursor
           emoji="❤️" 
-          count={20} 
+          count={6} 
           size={28}
           hideNative={false}
-        /> */}
+        />
       </Suspense>
       <Suspense fallback={null}>
         <HeroSection />
