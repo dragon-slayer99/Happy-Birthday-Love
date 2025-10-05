422e632578431c454c496f2e1c9e3659c4741533
dragon-slayer99 <guudurumahesh@gmail.com>
2025-10-06 00:23:44 +0530

    Refactor code structure for improved readability and maintainability

--- src/Components/FinalWords/FinalWords.jsx
+++ src/Components/FinalWords/FinalWords.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import './FinalWords.css';
+import together from '../../assets/images/Screenshot_2025-09-14-21-53-14-61.jpg'
 
 function FinalWords() {
     const [open, setOpen] = useState(false);
@@ -24,7 +25,7 @@ function FinalWords() {
                         onClick={(e) => e.stopPropagation()} // prevent closing on click inside
                     >
                         <h2>On this special day...</h2>
-                        <img src="/src/assets/images/Screenshot_2025-09-14-21-53-14-61_a63b0f8076346d26cbdc1b971a1da2a7.jpg" alt="picture of us together" />
+                        <img src={together} />
                         <p>
                             Thank you for always being there for me ❤️. Your presence, your voice 🎵, your smile; even just seeing you fills me with joy. When I think about you, there’s no ‘I’ only ‘we’ because you’ve become someone truly special, someone I want to share my future with.
                         <span style={{ fontWeight: 'bold', color: 'red', padding: '10px', display: 'block', textAlign: 'center' }}>I LOVE YOU❤️</span>
